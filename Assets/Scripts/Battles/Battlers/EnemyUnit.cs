9f7080121a2e28e8cc6e98023e6cd96b4bcb9dfd
sotaro <s.abe@gmail.com>
2024-10-23 23:47:53 +0900

    feat : ★ソウル追加

--- Assets/Scripts/Battles/Battlers/EnemyUnit.cs
+++ Assets/Scripts/Battles/Battlers/EnemyUnit.cs
@@ -6,6 +6,7 @@ using TMPro;
 
 public class EnemyUnit : BattleUnit
 {
+    // TODO : エナジーUIを追加
     [SerializeField] Image image;
     [SerializeField] TextMeshProUGUI nameText;
     [SerializeField] TextMeshProUGUI attackText;
