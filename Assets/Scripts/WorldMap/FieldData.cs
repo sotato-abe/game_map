2509030d5f7a17bf05d1e20f4ba0d49b392fa008
sotaro <s.abe@gmail.com>
2024-12-19 17:11:12 +0900

    feat : ポジション確認用のクラスを追加

---
+++ Assets/Scripts/WorldMap/FieldData.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+public class FieldData
+{
+    public FieldType type; // マップデータ
+    public int row;         // 行数
+    public int col;         // 列数
+}
---
+++ Assets/Scripts/WorldMap/WorldPosition.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+
+public class WorldPosition
+{
+    public int row;         // 行数
+    public int col;         // 列数
+}
