86fa715144c08d1c35aa0526028cc36c3e91534d
sotaro <s.abe@gmail.com>
2025-01-10 22:14:54 +0900

    feat : ステートUIを変更

--- Assets/Scripts/Action/ActionPanel.cs
+++ Assets/Scripts/Action/ActionPanel.cs
@@ -10,7 +10,6 @@ public class ActionPanel : MonoBehaviour
     [SerializeField] SelectableText selectableTextPrefab;
     [SerializeField] Image dialogBackground;
     [SerializeField] ActionBoard actionBoard;
-    [SerializeField] AutoPanel autoPanel;
     private List<SelectableText> actionTexts; // ActionList内のテキストリスト
     ActionType selectedAction;
     private Coroutine fadeCoroutine;
@@ -112,7 +111,6 @@ public class ActionPanel : MonoBehaviour
         {
             StopCoroutine(fadeCoroutine);
         }
-        autoPanel.SetPanelValidity(targetAlpha);
 
         // 新しいフェードコルーチンを開始
         fadeCoroutine = StartCoroutine(FadeToAlpha(targetAlpha, duration));
