e3bbd5f17268a8a5762cdc305fd8507a27989f20
sotaro <s.abe@gmail.com>
2025-01-10 23:15:21 +0900

    feat : アイテムダイヤログの処理修正

--- Assets/Scripts/Action/ItemPanel.cs
+++ Assets/Scripts/Action/ItemPanel.cs
@@ -10,13 +10,11 @@ public class ItemPanel : MonoBehaviour
     [SerializeField] GameObject itemList;
     [SerializeField] BattleUnit playerUnit;
 
-    int previousItem;
     int selectedItem;
 
     private void Init()
     {
         selectedItem = 0;
-        previousItem = selectedItem;
     }
 
     private void OnEnable()
@@ -59,6 +57,14 @@ public class ItemPanel : MonoBehaviour
     }
     public void SelectItem(bool selectDirection)
     {
+        // 現在選択中のアイテムがリスト内に存在するか確認
+        if (selectedItem >= 0 && selectedItem < itemList.transform.childCount)
+        {
+            var previousItemUnit = itemList.transform.GetChild(selectedItem).GetComponent<ItemUnit>();
+            previousItemUnit.Targetfoucs(false);
+        }
+
+        // 選択方向に応じてインデックスを変更
         if (selectDirection)
         {
             selectedItem++;
@@ -67,44 +73,58 @@ public class ItemPanel : MonoBehaviour
         {
             selectedItem--;
         }
-        selectedItem = Mathf.Clamp(selectedItem, 0, playerUnit.Battler.Inventory.Count - 1);
 
-        if (itemList.transform.childCount > 0 && previousItem != selectedItem)
+        // インデックスをリストの範囲内に制限
+        selectedItem = Mathf.Clamp(selectedItem, 0, itemList.transform.childCount - 1);
+
+        // 新しい選択中のアイテムをハイライト
+        if (selectedItem >= 0 && selectedItem < itemList.transform.childCount)
         {
-            var previousItemUnit = itemList.transform.GetChild(previousItem).GetComponent<ItemUnit>();
-            previousItemUnit.Targetfoucs(false);
-            var currentItemUnit = itemList.transform.GetChild(selectedItem).GetComponent<ItemUnit>();
-            currentItemUnit.Targetfoucs(true);
-            previousItem = selectedItem;
+            var selectedItemUnit = itemList.transform.GetChild(selectedItem).GetComponent<ItemUnit>();
+            selectedItemUnit.Targetfoucs(true);
         }
     }
 
     public void UseItem()
     {
         // 選択されたアイテムの ItemUnit を取得
-        var targetItemUnit = itemList.transform.GetChild(selectedItem).GetComponent<ItemUnit>();
-
-        if (targetItemUnit != null && targetItemUnit.Item != null) // ItemUnit とその Item が存在するかを確認
+        if (selectedItem >= 0 && selectedItem < itemList.transform.childCount)
         {
-            // アイテムを使用する処理をここに追加
-            // 例: playerUnit.Battler.UseItem(targetItemUnit.Item);
-            int MaxBattery = playerUnit.Battler.MaxBattery;
-            int battery = playerUnit.Battler.Battery + targetItemUnit.Item.Base.Battery;
-            battery = Mathf.Clamp(battery, 0, MaxBattery);
+            // 選択されたアイテムの ItemUnit を取得
+            var targetItemUnit = itemList.transform.GetChild(selectedItem).GetComponent<ItemUnit>();
 
-            int maxLife = playerUnit.Battler.MaxLife;
-            int life = playerUnit.Battler.Life + targetItemUnit.Item.Base.Life;
-            life = Mathf.Clamp(life, 0, maxLife);
+            if (targetItemUnit != null && targetItemUnit.Item != null) // ItemUnit とその Item が存在するかを確認
+            {
+                // アイテムを使用する処理をここに追加
+                // 例: playerUnit.Battler.UseItem(targetItemUnit.Item);
+                int MaxBattery = playerUnit.Battler.MaxBattery;
+                int battery = playerUnit.Battler.Battery + targetItemUnit.Item.Base.Battery;
+                battery = Mathf.Clamp(battery, 0, MaxBattery);
 
-            playerUnit.Battler.Life = life;
-            playerUnit.Battler.Battery = battery;
-            playerUnit.Battler.Inventory.Remove(targetItemUnit.Item);
-            SetItemDialog();
-            playerUnit.UpdateUI();
+                int maxLife = playerUnit.Battler.MaxLife;
+                int life = playerUnit.Battler.Life + targetItemUnit.Item.Base.Life;
+                life = Mathf.Clamp(life, 0, maxLife);
+
+                playerUnit.Battler.Life = life;
+                playerUnit.Battler.Battery = battery;
+                playerUnit.Battler.Inventory.Remove(targetItemUnit.Item);
+
+                selectedItem = Mathf.Clamp(selectedItem, 0, itemList.transform.childCount - 2);
+
+                var selectedItemUnit = itemList.transform.GetChild(selectedItem).GetComponent<ItemUnit>();
+                selectedItemUnit.Targetfoucs(false);
+
+                SetItemDialog();
+                playerUnit.UpdateUI();
+            }
+            else
+            {
+                Debug.LogWarning("No item found to use.");
+            }
         }
         else
         {
-            Debug.LogWarning("No item found to use.");
+            Debug.LogWarning("Selected item is out of bounds.");
         }
     }
 }
