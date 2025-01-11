c6404f075d5270e2502a5ca41dcff733e4c110f1
sotaro <s.abe@gmail.com>
2025-01-11 22:49:49 +0900

    feat : ステートパネルを最適化

--- Assets/Scripts/UI/AgeTimePanel.cs
+++ Assets/Scripts/UI/AgeTimePanel.cs
@@ -8,9 +8,6 @@ using System;
 public class AgeTimePanel : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI ageTimeField;  // 表示用のTextMeshProUGUIフィールド
-    [SerializeField] Image stopPanel;
-    [SerializeField] Image playPanel;
-    [SerializeField] Image fastPanel;
     [SerializeField] StatePanelController statePanel;
 
     private DateTime ageTime;        // 現在の時間
@@ -33,24 +30,6 @@ public class AgeTimePanel : MonoBehaviour
     {
         timeSpeed = state;
         statePanel.ChangeState(state);
-        if (state == TimeState.Stop)
-        {
-            stopPanel.gameObject.SetActive(true);
-            playPanel.gameObject.SetActive(false);
-            fastPanel.gameObject.SetActive(false);
-        }
-        else if (state == TimeState.Live)
-        {
-            stopPanel.gameObject.SetActive(false);
-            playPanel.gameObject.SetActive(true);
-            fastPanel.gameObject.SetActive(false);
-        }
-        else
-        {
-            stopPanel.gameObject.SetActive(false);
-            playPanel.gameObject.SetActive(false);
-            fastPanel.gameObject.SetActive(true);
-        }
     }
 
     // 時間経過を管理し、timeSpeedに応じて進行速度を変更する
