431ab8ceac77f08a60ce0d088d77049e2137bc8f
sotaro <s.abe@gmail.com>
2025-01-16 11:15:11 +0900

    refacrer : クラス名整理

--- Assets/Scripts/WorldMap/FieldData.cs
+++ Assets/Scripts/WorldMap/FieldData.cs
@@ -2,7 +2,9 @@ using System.Collections.Generic;
 
 public class FieldData
 {
-    public FieldType type; // マップデータ
-
-    public Coordinate coordinate;
+    public GroundType groundType; // グラウンドタイプ
+    public FloorType floorType; // フロアタイプ
+    public DirectionType roadDirection; // 道
+    public SpotType spot; // スポット（スポットが該当しないときは０）
+    public Coordinate coordinate; // 座標
 }
