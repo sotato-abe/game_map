211cae8623f90aefcf02d2321130c58a697d24b2
sotaro <s.abe@gmail.com>
2025-02-07 01:24:46 +0900

    WIP : TurnOrderSystem更新

--- Assets/Scripts/UI/TurnBattlerIcon/TurnBattlerIcon.cs
+++ Assets/Scripts/UI/TurnBattlerIcon/TurnBattlerIcon.cs
@@ -2,21 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
-using System;
+using UnityEngine.Events;
 
 public class TurnBattlerIcon : MonoBehaviour
 {
     [SerializeField] private Image iconImage;  // 表示用のTextMeshProUGUIフィールド
     private bool isActive = true;
+    private float moveSpeed = 300f;
+    private float targetPositionX = -630f;
+    public delegate void ExecuteTurnDelegate(TurnBattlerIcon turnBattlerIcon);
+    public event ExecuteTurnDelegate ExecuteTurn;
+
     public void SetCharacter(Sprite characterSprite)
     {
         iconImage.sprite = characterSprite;
+        StartCoroutine(MoveToLeft());
     }
 
-    public void SetActive(bool isActiveFlag)
+    public void SetActive(bool activeFlg)
     {
-        isActive = isActiveFlag;
+        isActive = activeFlg;
+    }
+
+    private IEnumerator MoveToLeft()
+    {
+
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        while (rectTransform.anchoredPosition.x > targetPositionX)
+        {
+            if (isActive)
+            {
+                rectTransform.anchoredPosition += Vector2.left * moveSpeed * Time.deltaTime;
+            }
+            yield return null;
+        }
+
+        // ターン開始を通知し、ターン終了後にこのオブジェクトを破棄
+        ExecuteTurn?.Invoke(this);
+        // ターン終了したTurnBattlerIconを削除
+        Destroy(this.gameObject);
     }
 }
 
