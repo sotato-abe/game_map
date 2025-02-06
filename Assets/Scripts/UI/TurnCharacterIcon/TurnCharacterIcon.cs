9b9dc0e2ecc5e17bfbf9e6c1a2367c56924d603e
sotaro <s.abe@gmail.com>
2025-02-06 21:21:26 +0900

    feat : TurnCharacterIcon追加

---
+++ Assets/Scripts/UI/TurnCharacterIcon/TurnCharacterIcon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class TurnCharacterIcon : MonoBehaviour
+{
+    [SerializeField] private Image iconImage;  // 表示用のTextMeshProUGUIフィールド
+}
