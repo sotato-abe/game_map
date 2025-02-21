91f5970634cf5a2ab506478aef478abf469edbf1
sotaro <s.abe@gmail.com>
2025-02-21 15:50:34 +0900

    feat : 装備の確率による実行を追加

--- Assets/Scripts/PlayerSystem/Action/AttackPanel.cs
+++ Assets/Scripts/PlayerSystem/Action/AttackPanel.cs
@@ -10,6 +10,8 @@ public class AttackPanel : MonoBehaviour
     [SerializeField] GameObject equipmentList;
     [SerializeField] BattleUnit playerUnit;
 
+    List<EquipmentUnit> equipmentUnitList = new List<EquipmentUnit>();
+
     private void Init()
     {
     }
@@ -39,8 +41,28 @@ public class AttackPanel : MonoBehaviour
             GameObject equipmentUnitObject = Instantiate(equipmentUnitPrefab, equipmentList.transform);
             equipmentUnitObject.gameObject.SetActive(true);
             EquipmentUnit equipmentUnit = equipmentUnitObject.GetComponent<EquipmentUnit>();
+            equipmentUnitList.Add(equipmentUnit);
 
             equipmentUnit.Setup(equipment);
         }
+    }
+
+    public List<Skill> ActivateEquipments()
+    {
+        List<Skill> skills = new List<Skill>();
+
+
+        foreach (EquipmentUnit equipment in equipmentUnitList)
+        {
+            if (Random.Range(0, 100) < equipment.Equipment.Base.Probability)
+            {
+                equipment.SetEquipmentMotion(EquipmentUnitMotionType.Jump);
+                skills.AddRange(equipment.Equipment.Base.SkillList);
+            }
+        }
+
+        Debug.Log($"ActivateEquipments:skills:{skills.Count}");
+
+        return skills;
     }
 }
