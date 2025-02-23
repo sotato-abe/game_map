66b7590ead2e08b04659f83d4df10740c8310feb
sotaro <s.abe@gmail.com>
2025-02-24 04:15:30 +0900

    feat : コマンドのDescriptionにエンチャントとコストを反映

--- Assets/Scripts/Data/Command/DescriptionPanel.cs
+++ Assets/Scripts/Data/Command/DescriptionPanel.cs
@@ -8,6 +8,11 @@ public class DescriptionPanel : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI commandName;
     [SerializeField] private TextMeshProUGUI description;
+    [SerializeField] GameObject enchantList;
+    [SerializeField] GameObject costList;
+    [SerializeField] EnchantIcon enchantPrefab;
+    [SerializeField] CostIcon costPrefab;
+
 
     void Start()
     {
@@ -18,10 +23,52 @@ public class DescriptionPanel : MonoBehaviour
     {
         commandName.text = command.Base.Name;
         description.text = command.Base.Description;
+        SetEnchant(command.Base.EnchantList);
+        SetCost(command.Base.CostList);
     }
 
     public void ShowDescriptionPanel(bool showFlg)
     {
         transform.gameObject.SetActive(showFlg);
+    }
+
+    private void SetEnchant(List<Enchant> enchants)
+    {
+        // enchantList内のオブジェクトを削除
+        foreach (Transform child in enchantList.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // enchantList内にスキルを追加
+        foreach (var enchant in enchants)
+        {
+            EnchantIcon enchantObject = Instantiate(enchantPrefab, enchantList.transform);
+            enchantObject.gameObject.SetActive(true);
+            EnchantIcon enchantUnit = enchantObject.GetComponent<EnchantIcon>();
+
+            enchantUnit.SetEnchant(enchant);
+        }
+    }
+
+    private void SetCost(List<Enegy> costs)
+    {
+        // costList内のオブジェクトを削除
+        foreach (Transform child in costList.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // costList内にコストを追加
+        foreach (var cost in costs)
+        {
+            if (0 < cost.val)
+            {
+                CostIcon costObject = Instantiate(costPrefab, costList.transform);
+                costObject.gameObject.SetActive(true);
+                CostIcon costUnit = costObject.GetComponent<CostIcon>();
+                costUnit.SetCostIcon(cost);
+            }
+        }
     }
 }
