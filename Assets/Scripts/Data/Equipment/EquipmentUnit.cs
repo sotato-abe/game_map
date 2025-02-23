0bbba943100cdaff74488181588580522d16e832
sotaro <s.abe@gmail.com>
2025-02-23 15:41:41 +0900

    feat : アイテムと装備にもフォーカスモーションを追加

--- Assets/Scripts/Data/Equipment/EquipmentUnit.cs
+++ Assets/Scripts/Data/Equipment/EquipmentUnit.cs
@@ -94,4 +94,47 @@ public class EquipmentUnit : MonoBehaviour
             transform.position = new Vector3(transform.position.x, groundY, transform.position.z);
         }
     }
+
+    public void OnPointerEnter()
+    {
+        Debug.Log("OnPointerEnter");
+        StartCoroutine(Targetfoucs(true));
+    }
+
+    public void OnPointerExit()
+    {
+        Debug.Log("OnPointerExit");
+        StartCoroutine(Targetfoucs(false));
+    }
+
+
+    public IEnumerator Targetfoucs(bool focusFlg)
+    {
+        float time = 0.05f;
+        float currentTime = 0f;
+        if (focusFlg)
+        {
+            Vector3 originalScale = transform.localScale;
+            Vector3 targetScale = new Vector3(1.1f, 1.1f, 1.1f);
+            while (currentTime < time)
+            {
+                transform.localScale = Vector3.Lerp(originalScale, targetScale, currentTime / time);
+                currentTime += Time.deltaTime;
+                yield return null;
+            }
+            transform.localScale = targetScale;
+        }
+        else
+        {
+            Vector3 originalScale = transform.localScale;
+            Vector3 targetScale = new Vector3(1, 1, 1);
+            while (currentTime < time)
+            {
+                transform.localScale = Vector3.Lerp(originalScale, targetScale, currentTime / time);
+                currentTime += Time.deltaTime;
+                yield return null;
+            }
+            transform.localScale = targetScale;
+        }
+    }
 }
