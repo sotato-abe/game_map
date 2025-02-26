b1b87a8025bb03a450cf4c2eaa64c14fd56dd418
sotaro <s.abe@gmail.com>
2025-02-26 21:48:05 +0900

    feat : Panelを各パネルに反映

--- Assets/Scripts/PlayerSystem/Action/DeckPanel.cs
+++ Assets/Scripts/PlayerSystem/Action/DeckPanel.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class DeckPanel : MonoBehaviour
+public class DeckPanel : Panel
 {
     
 }
--- Assets/Scripts/PlayerSystem/Action/EquipmentPanel.cs
+++ Assets/Scripts/PlayerSystem/Action/EquipmentPanel.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class EquipmentPanel : MonoBehaviour
+public class EquipmentPanel : Panel
 {
     
 }
