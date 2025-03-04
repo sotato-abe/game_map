9614677bc2d7ad8d52864531e06c0426cdd90504
sotaro <s.abe@gmail.com>
2025-03-04 20:20:58 +0900

    style : バトル後の待機時間を変更

--- Assets/Scripts/BattleSystem/BattleSystem.cs
+++ Assets/Scripts/BattleSystem/BattleSystem.cs
@@ -253,7 +253,7 @@ public class BattleSystem : MonoBehaviour
         {
             yield return StartCoroutine(messagePanel.TypeDialog(message));
         }
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1.5f);
         BattleEnd();
     }
 
