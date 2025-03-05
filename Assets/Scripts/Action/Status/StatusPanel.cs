69dc255db65798c1dd8d818648ad3c3b11fbb1c0
sotaro <s.abe@gmail.com>
2025-03-05 18:02:18 +0900

    feat : StatusPanelを追加

---
+++ Assets/Scripts/Action/Status/StatusPanel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+
+public class StatusPanel : Panel
+{
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Quit();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OnActionExit?.Invoke();
+        }
+    }
+
+    private void Quit()
+    {
+        isActive = false;
+        OnActionExecute?.Invoke();
+    }
+}
