91e2f959bde2a3bd1d0d506cdc66e57925ed05c1
sotaro <s.abe@gmail.com>
2025-03-07 13:03:55 +0900

    feat : 積載率とコストをPanelに反映

--- Assets/Scripts/Action/Deck/DeckPanel.cs
+++ Assets/Scripts/Action/Deck/DeckPanel.cs
@@ -11,6 +11,11 @@ public class DeckPanel : Panel
     [SerializeField] GameObject runTable;
     [SerializeField] GameObject deck;
     [SerializeField] GameObject storage;
+    [SerializeField] TextMeshProUGUI lifeCostTexe;
+    [SerializeField] TextMeshProUGUI batteryCostTexe;
+    [SerializeField] TextMeshProUGUI soulCostTexe;
+    [SerializeField] TextMeshProUGUI storageRatio;
+    [SerializeField] TextMeshProUGUI deckRatio;
     [SerializeField] BattleUnit playerUnit;
 
 
@@ -46,25 +51,45 @@ public class DeckPanel : Panel
 
     public void SetRunTable()
     {
-        foreach (Transform child in runTable.transform)
-        {
-            Destroy(child.gameObject);
-        }
+        int lifeCost = 0;
+        int batteryCost = 0;
+        int soulCost = 0;
+
+        ClearTransformChildren(runTable.transform);
 
         foreach (Command command in playerUnit.Battler.RunTable)
         {
             CommandSlot commandSlot = Instantiate(commandPrefab, runTable.transform);
             commandSlot.gameObject.SetActive(true);
             commandSlot.Setup(command);
+
+            foreach (Enegy cost in command.Base.CostList)
+            {
+                switch (cost.type)
+                {
+                    case EnegyType.Life:
+                        lifeCost += cost.val;
+                        break;
+                    case EnegyType.Battery:
+                        batteryCost += cost.val;
+                        break;
+                    case EnegyType.Soul:
+                        soulCost += cost.val;
+                        break;
+                }
+            }
         }
+        lifeCostTexe.SetText(lifeCost.ToString());
+        batteryCostTexe.SetText(batteryCost.ToString());
+        soulCostTexe.SetText(soulCost.ToString());
     }
 
     public void SetDeck()
     {
-        foreach (Transform child in deck.transform)
-        {
-            Destroy(child.gameObject);
-        }
+        int commandCount = playerUnit.Battler.RunTable.Count + playerUnit.Battler.DeckList.Count;
+        deckRatio.text = $"{commandCount}/{playerUnit.Battler.Memory.val}";
+
+        ClearTransformChildren(deck.transform);
 
         foreach (Command command in playerUnit.Battler.DeckList)
         {
@@ -76,16 +101,23 @@ public class DeckPanel : Panel
 
     public void SetStorage()
     {
-        foreach (Transform child in storage.transform)
-        {
-            Destroy(child.gameObject);
-        }
+        storageRatio.text = $"{playerUnit.Battler.StorageList.Count}/{playerUnit.Battler.Storage.val}";
+
+        ClearTransformChildren(storage.transform);
 
         foreach (Command command in playerUnit.Battler.StorageList)
         {
             CommandSlot commandSlot = Instantiate(commandPrefab, storage.transform);
             commandSlot.gameObject.SetActive(true);
             commandSlot.Setup(command);
+        }
+    }
+
+    private void ClearTransformChildren(Transform parent)
+    {
+        foreach (Transform child in parent)
+        {
+            Destroy(child.gameObject);
         }
     }
 }
