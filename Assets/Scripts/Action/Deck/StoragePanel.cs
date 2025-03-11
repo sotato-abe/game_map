cfae5f7f9cca840bee3c50c2d82282d03e7577d0
sotaro <s.abe@gmail.com>
2025-03-11 18:29:17 +0900

    feat : BagPanelに自動整列を追加

--- Assets/Scripts/Action/Deck/StoragePanel.cs
+++ Assets/Scripts/Action/Deck/StoragePanel.cs
@@ -4,31 +4,58 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
-public class StoragePanel : Panel
+public class StoragePanel : Panel, IDropHandler
 {
     [SerializeField] CommandSlot commandPrefab;  // CommandSlotのプレハブ
-    [SerializeField] GameObject storage;
+    [SerializeField] GameObject blockPrefab;  // blockのプレハブ
+    [SerializeField] GameObject storageArea;
     [SerializeField] TextMeshProUGUI storageRatio;
     [SerializeField] DeckWindow deckWindow;
     [SerializeField] BattleUnit playerUnit;
 
-    private int headHeight = 105;
+    private int headHeight = 40;
     private int commandWidth = 70;
-    int row = 5;
-    int padding = 0;
+    int row = 10;
+    int padding = 10;
     List<CommandSlot> storageList = new List<CommandSlot>();
+    private List<GameObject> blockList = new List<GameObject>();
 
     private void OnEnable()
     {
         if (playerUnit != null && playerUnit.Battler != null)
         {
             deckWindow.SetUp(playerUnit.Battler);
+            SetPanelSize();
             SetStorage();
         }
         else
         {
             Debug.LogWarning("playerUnit or its properties are not initialized.");
+        }
+    }
+
+    // CommandSlotがドロップされたときの処理
+    public void OnDrop(PointerEventData eventData)
+    {
+        CommandSlot droppedCommandSlot = eventData.pointerDrag.GetComponent<CommandSlot>();
+
+        if (droppedCommandSlot != null)
+        {
+            if (playerUnit.Battler.StorageList.Contains(droppedCommandSlot.command))
+            {
+                Debug.Log("コマンドはすでにデッキに存在しています。");
+                return; // 追加しない
+            }
+            if (playerUnit.Battler.StorageList.Count >= playerUnit.Battler.Storage.val)
+            {
+                Debug.Log("ストレージがいっぱいです。");
+                return; // 追加しない
+            }
+
+            AddCommandSlot(droppedCommandSlot.command); // バッグに追加
+            deckWindow.RemoveCommand(droppedCommandSlot);
         }
     }
 
@@ -48,24 +75,54 @@ public class StoragePanel : Panel
         }
     }
 
+    public void SetPanelSize()
+    {
+        int width = commandWidth * row + 30;
+        int column = (playerUnit.Battler.Storage.val - 1) / row + 1;
+        int height = commandWidth * column + headHeight;
+        GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+    }
+
     public void SetStorage()
     {
-        storageRatio.text = $"{playerUnit.Battler.StorageList.Count}/{playerUnit.Battler.Storage.val}";
-
         storageList.Clear();
-        ClearTransformChildren(storage.transform);
+        foreach (Transform child in storageArea.transform)
+        {
+            Destroy(child.gameObject);
+        }
 
         foreach (Command command in playerUnit.Battler.StorageList)
         {
-            CommandSlot commandSlot = Instantiate(commandPrefab, storage.transform);
+            CommandSlot commandSlot = Instantiate(commandPrefab, storageArea.transform);
             commandSlot.gameObject.SetActive(true);
             commandSlot.OnEndDragAction += ArrengeStorage; // 正しく登録
             commandSlot.Setup(command);
             storageList.Add(commandSlot);
         }
+        SetBlock();
+        ArrengeStorage();
+        storageRatio.text = $"{playerUnit.Battler.StorageList.Count}/{playerUnit.Battler.Storage.val}";
     }
 
-    public void RemoveCommand(CommandSlot commandSlot)
+    private void SetBlock()
+    {
+        int blockNum = row - playerUnit.Battler.Storage.val % row;
+        blockList.Clear();
+        for (int i = 0; i < blockNum; i++)
+        {
+            GameObject blockObject = Instantiate(blockPrefab, storageArea.transform);
+            blockObject.gameObject.SetActive(true);
+            blockList.Add(blockObject);
+        }
+    }
+
+    public void AddCommandSlot(Command command)
+    {
+        playerUnit.Battler.StorageList.Add(command);
+        SetStorage();
+    }
+
+    public void RemoveCommandSlot(CommandSlot commandSlot)
     {
         playerUnit.Battler.StorageList.Remove(commandSlot.command);
         SetStorage();
@@ -74,13 +131,21 @@ public class StoragePanel : Panel
     private void ArrengeStorage()
     {
         Debug.Log("ArrengeStorage");
-    }
-
-    private void ClearTransformChildren(Transform parent)
-    {
-        foreach (Transform child in parent)
+        storageList.RemoveAll(command => command == null); // 破棄されたオブジェクトを削除
+        for (int i = 0; i < storageList.Count; i++)
         {
-            Destroy(child.gameObject);
+            int cardHalfWidth = commandWidth / 2;
+            int xPosition = (i % row) * commandWidth + cardHalfWidth + padding;
+            int yPosition = -cardHalfWidth - padding;
+            storageList[i].transform.localPosition = new Vector3(xPosition, yPosition, 0);
+        }
+
+        for (int i = 0; i < blockList.Count; i++)
+        {
+            int cardHalfWidth = commandWidth / 2;
+            int xPosition = (playerUnit.Battler.Storage.val % row + i) * commandWidth + cardHalfWidth + padding;
+            int yPosition = -((playerUnit.Battler.Storage.val / row) * commandWidth + cardHalfWidth) - padding;
+            blockList[i].transform.localPosition = new Vector3(xPosition, yPosition, 0);
         }
     }
 }
