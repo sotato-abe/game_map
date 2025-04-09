0e1fd025f224d4ae034615f6d885df6a08e54850
sotaro <s.abe@gmail.com>
2025-04-09 20:34:26 +0900

    feat : FielsInfoPanelのtitleをリサイズするように変更

--- Assets/Scripts/Components/Title.cs
+++ Assets/Scripts/Components/Title.cs
@@ -8,10 +8,13 @@ public class Title : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI title;
     [SerializeField] Image image;
+    [SerializeField] float padding = 30f;
+
     private RectTransform backImageRectTransform;
 
     private void Start()
     {
+        backImageRectTransform = GetComponent<RectTransform>();
     }
 
     public IEnumerator TypeTitle(string line)
@@ -22,5 +25,18 @@ public class Title : MonoBehaviour
             title.text += letter;
             yield return new WaitForSeconds(0f);
         }
+        ResizePlate();
+    }
+
+    private void ResizePlate()
+    {
+        if (title == null || backImageRectTransform == null)
+        {
+            Debug.LogError("title または backImageRectTransform が null");
+            return;
+        }
+
+        float newWidth = title.preferredWidth + padding;
+        backImageRectTransform.sizeDelta = new Vector2(newWidth, backImageRectTransform.sizeDelta.y);
     }
 }
