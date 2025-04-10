076357a406c547d68e82b096a2c3a05e5713110b
sotaro <s.abe@gmail.com>
2025-04-10 19:10:00 +0900

    fix : StatusPanelのレベル関連の挙動を修正

--- Assets/Scripts/Action/Status/StatusLevel.cs
+++ Assets/Scripts/Action/Status/StatusLevel.cs
@@ -24,6 +24,7 @@ public class StatusLevel : MonoBehaviour
         {
             expText.text = exp.ToString() + "/100";
         }
+        if (currentLevel == level && currentExp == exp) return; // 変更がない場合は処理しない
         UpdateExpBar(level, exp);
     }
 
@@ -32,5 +33,7 @@ public class StatusLevel : MonoBehaviour
         // 以前からのレベル差と新しい経験値を受け渡す
         int levelDifference = level - currentLevel;
         bar.SetExpBar(levelDifference, exp);
+        currentLevel = level; // 現在のレベルを更新
+        currentExp = exp; // 現在の経験値を更新
     }
 }
