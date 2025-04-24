c2ef842d5a02f78fd6bf504e659986d21880faf2
sotaro <s.abe@gmail.com>
2025-04-24 23:32:08 +0900

    feat : StatusPanelのUI変更

--- Assets/Scripts/Data/Battler/Ability/AbilityUnit.cs
+++ Assets/Scripts/Data/Battler/Ability/AbilityUnit.cs
@@ -8,8 +8,15 @@ public class AbilityUnit : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI name;  // 表示用のTextMeshProUGUIフィールド
     [SerializeField] private TextMeshProUGUI description;  // 表示用のTextMeshProUGUIフィールド
+    // private Canvas abilityCanvas;  // 表示用のTextMeshProUGUIフィールド
     private bool isActive = false;
 
+    private float magnificationPower = 2.0f; // ターゲットスケール
+    // void Awake()
+    // {
+    //     if (abilityCanvas == null)
+    //         abilityCanvas = GetComponent<Canvas>(); // 自分にCanvasがある場合、自動取得
+    // }
     public virtual void Setup(Ability ability)
     {
         name.text = ability.Name;
@@ -18,8 +25,8 @@ public class AbilityUnit : MonoBehaviour
 
     public void OnPointerEnter()
     {
+        Debug.Log("OnPointerEnter");
         StartCoroutine(OnPointer(true));
-
     }
 
     public void OnPointerExit()
@@ -33,8 +40,11 @@ public class AbilityUnit : MonoBehaviour
         float currentTime = 0f;
         if (focusFlg)
         {
+            // abilityCanvas.overrideSorting = true;
+            // abilityCanvas.sortingOrder = 10;
+
             Vector3 originalScale = transform.localScale;
-            Vector3 targetScale = new Vector3(1.1f, 1.1f, 1.1f);
+            Vector3 targetScale = new Vector3(magnificationPower, magnificationPower, magnificationPower);
             while (currentTime < time)
             {
                 transform.localScale = Vector3.Lerp(originalScale, targetScale, currentTime / time);
@@ -54,6 +64,7 @@ public class AbilityUnit : MonoBehaviour
                 yield return null;
             }
             transform.localScale = targetScale;
+            // abilityCanvas.sortingOrder = 0;
         }
     }
 
