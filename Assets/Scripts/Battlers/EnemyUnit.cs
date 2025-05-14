c2f97877111c0fc00f25279bf389373ed9ea0e46
sotaro <s.abe@gmail.com>
2025-05-14 21:32:21 +0900

    feat：BattlerUnitにNamePlateを追加

--- Assets/Scripts/Battlers/EnemyUnit.cs
+++ Assets/Scripts/Battlers/EnemyUnit.cs
@@ -6,10 +6,13 @@ using TMPro;
 
 public class EnemyUnit : BattleUnit
 {
+    [SerializeField] public NamePlate namePlate;
+
     public override void Setup(Battler battler)
     {
         battler.Init();
         base.Setup(battler);
+        namePlate.SetName(battler.Base.Name);
     }
 
     public override void UpdateEnegyUI()
