0d6f336a2241fef2c8bbb3ecad71d988458c6f3a
sotaro <s.abe@gmail.com>
2025-06-08 23:44:13 +0900

    style : TitlePnaelのスタイル変更

--- Assets/Scripts/Components/TitlePanel.cs
+++ Assets/Scripts/Components/TitlePanel.cs
@@ -8,9 +8,14 @@ public class TitlePanel : SlidePanel
 {
     [SerializeField] private TextMeshProUGUI nameText;
     [SerializeField] Image iconImage;
+    [SerializeField] Image backPanelImage;
     [SerializeField] Sprite reserveIcon;
     [SerializeField] Sprite battleIcon;
     [SerializeField] Sprite configIcon;
+
+    Color32 reserveColor = new Color32(187, 0, 255, 200);
+    Color32 battleColor = new Color32(255, 0, 71, 200);
+    Color32 settingColor = new Color32(0, 237, 255, 200);
 
     public void SetTitle(TitleType titleType)
     {
@@ -18,15 +23,18 @@ public class TitlePanel : SlidePanel
         {
             case TitleType.Reserve:
                 iconImage.sprite = reserveIcon;
-                nameText.SetText("リザーブ");
+                nameText.SetText("準備");
+                backPanelImage.color = reserveColor;
                 break;
             case TitleType.Battle:
                 iconImage.sprite = battleIcon;
-                nameText.SetText("バトル");
+                nameText.SetText("戦闘");
+                backPanelImage.color = battleColor;
                 break;
             case TitleType.Config:
                 iconImage.sprite = configIcon;
                 nameText.SetText("設定");
+                backPanelImage.color = settingColor;
                 break;
             default:
                 Debug.LogError("Unknown TitleType: " + titleType);
